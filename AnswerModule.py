1562f5c40721b98da321f81e1c55863238ac6cb0
Ali <ali.sarmadi6106@gmail.com>
2023-11-27 00:32:20 +0330

    Initial Commit

---
+++ AnswerModule.py
@@ -0,0 +1,38 @@
+import torch
+
+from torch import nn
+
+
+class AnswerModule(nn.Module):
+    '''
+    This Answer Module is specifically designed for bAbI dataset in which answers
+    has only one word.
+    '''
+
+    def __init__(self, embedding_size, middle_size, vocab_size):
+        super().__init__()
+        print(f'vocab size: {vocab_size}')
+        self.embedding_size = embedding_size
+        self.middle_size = middle_size
+        self.gru = nn.GRU(input_size=embedding_size, hidden_size=embedding_size,
+                          num_layers=1, batch_first=True)
+        self.softmax = nn.Softmax()
+        self.linear_layer_1 = nn.Linear(in_features=embedding_size, out_features=vocab_size)
+        self.linear_layer_2 = nn.Linear(in_features=middle_size + 1, out_features=1)
+
+    def forward(self, m0, q):
+        a0 = m0[:, -1, :]
+
+        # y0 = self.softmax(self.linear_layer(a0))
+
+        # print(f'y0 shape: {y0.shape}')
+
+        concatenated_tensor = torch.cat((q, a0.unsqueeze(dim=1)), dim=1)
+        a1 = self.gru(concatenated_tensor, a0.unsqueeze(dim=0))[0]
+
+        intermediate_a1 = self.linear_layer_1(a1)
+        upper_intermediate_a1 = self.linear_layer_2(torch.transpose(intermediate_a1, 1, 2))
+        advanced_a1 = upper_intermediate_a1.squeeze(dim=-1)
+        y1 = self.softmax(advanced_a1)
+
+        return y1 
---
+++ FeatureVector.py
@@ -0,0 +1,32 @@
+import torch
+
+from torch import nn
+
+
+class FeatureVector(nn.Module):
+
+    def __init__(self, embedding_size, middle_number):
+        super().__init__()
+
+        self.weight = nn.Linear(in_features=embedding_size, out_features=embedding_size)
+        self.size_of_scoring_network_layer = 7 * embedding_size * middle_number + 2 * embedding_size * embedding_size
+        self.layer_1 = nn.Linear(in_features=self.size_of_scoring_network_layer,
+                                 out_features=self.size_of_scoring_network_layer)
+        self.layer_2 = nn.Linear(in_features=self.size_of_scoring_network_layer, out_features=1)
+        self.fnn = nn.Sequential(
+            self.layer_1,
+            nn.Tanh(),
+            self.layer_2,
+            nn.Sigmoid()
+        )
+
+    def forward(self, c, m, q):
+        weighted_c = self.weight(c)
+
+        cwm = torch.matmul(torch.transpose(weighted_c, 1, 2), m)
+        cwq = torch.matmul(torch.transpose(weighted_c, 1, 2), q)
+        flatten = nn.Flatten(start_dim=1, end_dim=-1)
+        z = torch.cat((flatten(c), flatten(m), flatten(q), flatten(torch.mul(c, q)), flatten(torch.mul(c, m)),
+                       flatten(torch.abs(torch.sub(c, q))), flatten(torch.abs(torch.sub(c, m))), flatten(cwq),
+                       flatten(cwm)), 1)
+        return self.fnn(z)
